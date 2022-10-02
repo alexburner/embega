da5ea013a27a7ce8743b6bb49b6bc8238b546dca
Alex Burner <alex.burner@gmail.com>
2022-10-02 13:19:02 -0700

    Add full-height hover tooltip to vis

--- app/components/stock-vis.ts
+++ app/components/stock-vis.ts
@@ -15,9 +15,11 @@ export default class StockVis extends Component<S> {
     // spec via:
     // - https://vega.github.io/vega-lite/examples/layer_candlestick.html
     // - https://vega.github.io/vega-lite/examples/interactive_overview_detail.html
+    // - https://vega.github.io/vega-lite/examples/interactive_histogram_full_height_hover.html
     embed(element, {
       $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
       data: { values: stockData },
+      transform: [{ calculate: 'datum.close - datum.open', as: 'delta' }],
       vconcat: [
         {
           width: chartWidth,
@@ -29,31 +31,79 @@ export default class StockVis extends Component<S> {
               title: '',
               scale: { domain: { param: 'brush' } },
             },
-            y: {
-              type: 'quantitative',
-              axis: { title: 'Price' },
-            },
-            color: {
-              condition: {
-                test: 'datum.open < datum.close',
-                value: '#06982d',
-              },
-              value: '#ae1325',
-            },
           },
           layer: [
             {
+              params: [
+                {
+                  name: 'hover',
+                  select: {
+                    type: 'point',
+                    on: 'mouseover',
+                    clear: 'mouseout',
+                  },
+                },
+              ],
+              mark: {
+                type: 'bar',
+                color: '#eee',
+                tooltip: true,
+              },
+              encoding: {
+                y: { value: 600 },
+                y2: { value: 0 },
+                opacity: {
+                  condition: {
+                    test: { param: 'hover', empty: false },
+                    value: 0.5,
+                  },
+                  value: 0,
+                },
+                tooltip: [
+                  { field: 'date', type: 'temporal' },
+                  { field: 'open', type: 'quantitative', format: '$,.2f' },
+                  { field: 'close', type: 'quantitative', format: '$,.2f' },
+                  { field: 'delta', type: 'quantitative', format: '+$,.2f' },
+                  { field: 'high', type: 'quantitative', format: '$,.2f' },
+                  { field: 'low', type: 'quantitative', format: '$,.2f' },
+                  { field: 'volume', type: 'quantitative', format: ',~r' },
+                ],
+              },
+            },
+            {
               mark: 'rule',
               encoding: {
-                y: { field: 'low' },
+                y: {
+                  field: 'low',
+                  type: 'quantitative',
+                  axis: { title: 'Price' },
+                },
                 y2: { field: 'high' },
+                color: {
+                  condition: {
+                    test: 'datum.open < datum.close',
+                    value: '#06982d',
+                  },
+                  value: '#ae1325',
+                },
               },
             },
             {
               mark: 'bar',
               encoding: {
-                y: { field: 'open' },
+                y: {
+                  field: 'open',
+                  type: 'quantitative',
+                  axis: { title: 'Price' },
+                },
                 y2: { field: 'close' },
+                color: {
+                  condition: {
+                    test: 'datum.open < datum.close',
+                    value: '#06982d',
+                  },
+                  value: '#ae1325',
+                },
               },
             },
           ],
