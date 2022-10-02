b1e9f1528af6164155fe5b85bcc5237063da4255
Alex Burner <alex.burner@gmail.com>
2022-10-02 12:33:01 -0700

    Add "Overiew and Detail" brushing to vis

--- app/components/stock-vis.ts
+++ app/components/stock-vis.ts
@@ -12,44 +12,95 @@ export default class StockVis extends Component<S> {
     const elementWidth = element.offsetWidth;
     const yAxisWidth = 50;
     const chartWidth = elementWidth - yAxisWidth;
-    // spec via https://vega.github.io/vega-lite/examples/layer_candlestick.html
+    // spec via:
+    // - https://vega.github.io/vega-lite/examples/layer_candlestick.html
+    // - https://vega.github.io/vega-lite/examples/interactive_overview_detail.html
     embed(element, {
       $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-      width: chartWidth,
       data: { values: stockData },
-      encoding: {
-        x: {
-          field: 'date',
-          type: 'temporal',
-          title: 'Date',
-        },
-        y: {
-          type: 'quantitative',
-          scale: { zero: false },
-          axis: { title: 'Price' },
-        },
-        color: {
-          condition: {
-            test: 'datum.open < datum.close',
-            value: '#06982d',
-          },
-          value: '#ae1325',
-        },
-      },
-      layer: [
+      vconcat: [
         {
-          mark: 'rule',
+          width: chartWidth,
+          height: 600,
           encoding: {
-            y: { field: 'low' },
-            y2: { field: 'high' },
+            x: {
+              field: 'date',
+              type: 'temporal',
+              title: '',
+              scale: { domain: { param: 'brush' } },
+            },
+            y: {
+              type: 'quantitative',
+              axis: { title: 'Price' },
+            },
+            color: {
+              condition: {
+                test: 'datum.open < datum.close',
+                value: '#06982d',
+              },
+              value: '#ae1325',
+            },
           },
+          layer: [
+            {
+              mark: 'rule',
+              encoding: {
+                y: { field: 'low' },
+                y2: { field: 'high' },
+              },
+            },
+            {
+              mark: 'bar',
+              encoding: {
+                y: { field: 'open' },
+                y2: { field: 'close' },
+              },
+            },
+          ],
         },
         {
-          mark: 'bar',
+          width: chartWidth,
+          height: 60,
           encoding: {
-            y: { field: 'open' },
-            y2: { field: 'close' },
+            x: {
+              field: 'date',
+              type: 'temporal',
+              title: 'Date',
+            },
+            y: {
+              type: 'quantitative',
+              axis: { title: '' },
+            },
+            color: {
+              condition: {
+                test: 'datum.open < datum.close',
+                value: '#06982d',
+              },
+              value: '#ae1325',
+            },
           },
+          layer: [
+            {
+              mark: 'rule',
+              encoding: {
+                y: { field: 'low' },
+                y2: { field: 'high' },
+              },
+              params: [
+                {
+                  name: 'brush',
+                  select: { type: 'interval', encodings: ['x'] },
+                },
+              ],
+            },
+            {
+              mark: 'bar',
+              encoding: {
+                y: { field: 'open' },
+                y2: { field: 'close' },
+              },
+            },
+          ],
         },
       ],
     });
